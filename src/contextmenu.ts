d255b0cfb50fc490f3e096493b7a792e9ca1a726
kelsny <cursors.owo.uwu@gmail.com>
2022-11-21 17:44:42 -0600

    context menu stuff

--- src/contextmenu.ts
+++ src/contextmenu.ts
@@ -1,11 +1,13 @@
-import { chips } from "./chips";
+import { Chip, chips } from "./chips";
 import { Component } from "./Component";
 import { INPUT_COMPONENT_CSS_SIZE, OUTPUT_COMPONENT_CSS_SIZE } from "./constants";
 import { DraggingManager } from "./DraggingManager";
+import { fromFile, saveDiagram } from "./files";
 import { Input } from "./Input";
 import { MenuManager } from "./MenuManager";
 import { Output } from "./Output";
 import { Reified } from "./Reified";
+import { WiringManager } from "./WiringManager";
 
 export const [queueNewContext] = MenuManager.use(Reified.root, [
     {
@@ -70,17 +72,91 @@ export const [queueNewContext] = MenuManager.use(Reified.root, [
     {
         "new-chip": {
             label: "New chip from diagram",
-            callback: () => {},
+            callback: () => {
+                //TODO: add modal/toast system
+
+                const name = prompt("Enter the name of the chip:");
+
+                if (!name) return;
+
+                if (
+                    !chips.has(name.toUpperCase()) &&
+                    !confirm("A chip already exists with this name. Are you sure you want to replace it?")
+                )
+                    return;
+
+                chips.set(
+                    name.toUpperCase(),
+                    class _ extends Chip<number, number> {
+                        constructor() {
+                            super(
+                                name!,
+                                [...Reified.active.values()].filter((v) => v instanceof Input).length,
+                                [...Reified.active.values()].filter((v) => v instanceof Output).length
+                            );
+                        }
+
+                        output(inputs: boolean[]): boolean[] {
+                            //TODO: SOMEHOW COMPILE THE DIAGRAM
+
+                            return [];
+                        }
+                    }
+                );
+            },
         },
     },
     {
         "save-as": {
             label: "Save as file",
-            callback: () => {},
+            callback: () => {
+                Object.assign(document.createElement("a"), {
+                    href: URL.createObjectURL(
+                        new Blob([saveDiagram([...Reified.active], WiringManager.wires)], { type: "application/json" })
+                    ),
+                    download: `gatesim-${Date.now()}.json`,
+                }).click();
+            },
         },
         "import-from": {
             label: "Import from file",
-            callback: () => {},
+            callback: async () => {
+                const input = Object.assign(document.createElement("input"), { type: "file" });
+
+                input.click();
+
+                const file = await new Promise<File | undefined>((resolve) => {
+                    input.onchange = () => resolve(input.files?.[0] ?? undefined);
+
+                    input.onerror = () => resolve(undefined);
+                });
+
+                if (!file) return;
+
+                const reader = new FileReader();
+
+                reader.readAsText(file);
+
+                const raw = await new Promise<string | undefined>((resolve) => {
+                    reader.onload = () => resolve(reader.result?.toString() ?? undefined);
+
+                    reader.onerror = () => resolve(undefined);
+                });
+
+                if (!raw) return;
+
+                //
+                console.log(raw);
+                //
+
+                const [components, wires] = fromFile(raw);
+
+                Reified.active.forEach((component) => component.detach());
+
+                Reified.active = new Set(components);
+
+                WiringManager.wires = wires;
+            },
         },
     },
 ]);
---
+++ src/files.ts
@@ -0,0 +1,12 @@
+import { Reified } from "./Reified";
+import { Wiring } from "./WiringManager";
+
+//TODO: make this work
+export function saveDiagram(components: Reified[], wires: Wiring[]) {
+    return "";
+}
+
+//TODO: make this work
+export function fromFile(file: string) {
+    return [];
+}
