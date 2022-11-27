3feb3e6b05ddea41911595466b8ba3d4340c3970
kelsny <cursors.owo.uwu@gmail.com>
2022-11-27 16:41:03 -0600

    moved stylesheet links to js

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { keybinds } from "./keybinds";
 import { SandboxManager } from "./managers/SandboxManager";
 import { menu } from "./menu";
+import { loadStyles } from "./styles";
+
+await loadStyles();
 
 SandboxManager.setup({ keybinds, menu, save: "sandbox" });
