69863201d07642f4a05d547a676463d34b9dd2ac
kelsny <cursors.owo.uwu@gmail.com>
2022-12-07 18:18:25 -0600

    select all keybind

--- src/keybinds/keybinds.ts
+++ src/keybinds/keybinds.ts
@@ -2,6 +2,7 @@ import { backspace } from "./backspace";
 import { behavior } from "./behavior";
 import { history } from "./history";
 import { io } from "./io";
+import { select } from "./select";
 import { serde } from "./serde";
 
 export const keybinds: Record<string, (e: KeyboardEvent) => void> = Object.assign(
@@ -10,5 +11,6 @@ export const keybinds: Record<string, (e: KeyboardEvent) => void> = Object.assig
     behavior,
     history,
     io,
+    select,
     serde,
 );
---
+++ src/keybinds/select.ts
@@ -0,0 +1,17 @@
+import { IS_MAC_OS } from "../constants";
+import { KeybindsManager } from "../managers/KeybindsManager";
+import { SelectionManager } from "../managers/SelectionManager";
+import { Reified } from "../reified/Reified";
+
+export const select = {
+    ...KeybindsManager.assign("Control+A", () => {
+        if (IS_MAC_OS) return;
+
+        Reified.active.forEach((component) => SelectionManager.addSelection(component));
+    }),
+    ...KeybindsManager.assign("Meta+A", () => {
+        if (!IS_MAC_OS) return;
+
+        Reified.active.forEach((component) => SelectionManager.addSelection(component));
+    }),
+} satisfies Record<string, (e: KeyboardEvent) => void>;
