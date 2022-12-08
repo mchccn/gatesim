f5904fb5ea2a438953612d51383357f39eb07311
kelsny <cursors.owo.uwu@gmail.com>
2022-12-07 22:58:18 -0600

    clean wp conf

--- webpack.config.js
+++ webpack.config.js
@@ -5,17 +5,8 @@ module.exports = {
     entry: "./src/index.ts",
     devtool: "inline-source-map",
     mode: "development",
-    module: {
-        rules: [{ test: /\.ts$/, use: "ts-loader", exclude: [path.resolve(__dirname, "node_modules")] }],
-    },
-    resolve: {
-        extensions: [".ts"],
-    },
-    output: {
-        filename: "index.js",
-        path: path.resolve(__dirname, "dist"),
-    },
-    experiments: {
-        topLevelAwait: true,
-    },
+    module: { rules: [{ test: /\.ts$/, use: "ts-loader", exclude: [path.resolve(__dirname, "node_modules")] }] },
+    resolve: { extensions: [".ts"] },
+    output: { filename: "index.js", path: path.resolve(__dirname, "dist") },
+    experiments: { topLevelAwait: true },
 };
