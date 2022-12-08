f8ac02c309a5b011687480070356b0aa95d54d24
kelsny <cursors.owo.uwu@gmail.com>
2022-12-08 17:21:52 -0600

    disable async chunks

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,6 @@ module.exports = {
     mode: "development",
     module: { rules: [{ test: /\.ts$/, use: "ts-loader", exclude: [path.resolve(__dirname, "node_modules")] }] },
     resolve: { extensions: [".ts"] },
-    output: { filename: "index.js", path: path.resolve(__dirname, "dist") },
+    output: { filename: "index.js", path: path.resolve(__dirname, "dist"), asyncChunks: false },
     experiments: { topLevelAwait: true },
 };
